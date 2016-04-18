4030fd182fbe96466c6a57f63c0be654f08ccf69
Alvin Grant <alving.nyc@gmail.com>
2016-04-18 12:55:42 -0400

    Register new sidebar
    Add lightbox for newsletter subscription
    
    Add functionality to add and check for cookies

--- js/scripts.js
+++ js/scripts.js
@@ -44,4 +44,37 @@
 	$(window).scroll(function(){
 		checkAnimation();
 	});
+
+	function setCookie(cname, cvalue, exdays) {
+		var d = new Date();
+		d.setTime(d.getTime() + (exdays*24*60*60*1000));
+		var expires = "expires="+d.toUTCString();
+		document.cookie = cname + "=" + cvalue + "; " + expires;
+	}
+
+	function getCookie(cname) {
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0; i<ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1);
+			if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+		}
+		return "";
+	}
+
+	$(window).load(function() {
+		var newsletter = getCookie("newsletter");
+		if (newsletter != "") {
+			//alert("Welcome again " + username);
+		} else{
+			$('.subscribe-box').fadeIn();
+		}
+	});
+
+	$('.close').on('click', function() {
+		$('.subscribe-box').fadeOut();
+		setCookie('newsletter', 'optout', 3);
+	});
+	
 }) (jQuery);
