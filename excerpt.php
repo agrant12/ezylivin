8c2058bbfbc45475e08dd915472f22be5b9e4c57
Alvin Grant <alving.nyc@gmail.com>
2016-04-30 12:44:40 -0400

    UI updates and mobile style changes

--- excerpt.php
+++ excerpt.php
@@ -8,5 +8,7 @@
 ?>
 <div class="entry clearfix">
 	<?php the_excerpt(); ?>
+	<div class="continue-reading"><a href="<?php the_permalink(); ?>">Continue Reading</a></div>
+	<span class="comment-home"> <?php comments_popup_link( __( 'No Comments', 'bootstrapcanvaswp' ), __( '1 Comment', 'bootstrapcanvaswp' ), __( '% Comments', 'bootstrapcanvaswp' ) ); ?></span>
 </div>
 		
